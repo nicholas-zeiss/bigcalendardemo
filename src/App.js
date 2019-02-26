6b206d9c4a873861f1667279fe101fb8a6a21f50
nicholas-zeiss <nicholas.zeiss@gmail.com>
2019-02-26 12:16:49 -0500

    clean up

--- src/App.js
+++ src/App.js
@@ -1,50 +1,61 @@
+// Proof of concept for the react-big-calendar package
+
 import React, { Component } from 'react';
 import './App.css';
 
 import BigCalendar from 'react-big-calendar'
 import moment from 'moment'
 
+
 const localizer = BigCalendar.momentLocalizer(moment)
 
-let first = true;
+
+// BigCalendar doesn't play well with the intial page render (its container needs to have well defined dimensions)
+// so as a quick hacky work around the first render of this component shows a loading message
+let firstRender = true;
 
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      date: new Date(),
-      events: []
-    }
+      events: [],
+    };
 
     this.handleSelectEvent = this._handleSelectEvent.bind(this);
     this.handleSelectSlot = this._handleSelectSlot.bind(this);
   }
 
+
   _handleSelectEvent(event) {
     this.setState(state => ({
-      events: state.events.filter(ev => ev !== event)
+      events: state.events.filter(ev => !this.eventsEqual(ev, event))
     }));
   }
 
+
+  // Handles event creation
   _handleSelectSlot({ start, end }) {
+    // Don't allow new events to start in the past or have no duration (which is how all day events work in BigCalendar)
     if (start < new Date() || start.getTime() === end.getTime()) {
       return;
     }
 
     this.setState(state => {
-      const mergedSlots = this.mergeSlots([
+      const mergedEvents = this.mergeEvents([
         ...state.events.map(ev => ({ ...ev })),
         { title: 'Available', start, end }
       ]);
 
-      return { events: mergedSlots }
+      return { events: mergedEvents }
     });
   }
 
-  mergeSlots(slots) {
+
+  mergeEvents(events) {
     const merged = [];
 
-    slots.sort((a,b) => {
+    events.sort((a, b) => {
       if (a.start >= b.start) {
         return -1;
       } else if (a.start < b.start) {
@@ -54,11 +65,11 @@ class App extends Component {
       }
     });
 
-    merged.push(slots.pop());
+    merged.push(events.pop());
 
-    while (slots.length) {
+    while (events.length) {
       const prev = merged[merged.length - 1];
-      const next = slots.pop();
+      const next = events.pop();
 
       if (next.start <= prev.end) {
         prev.end = next.end >= prev.end ? next.end : prev.end;
@@ -67,8 +78,9 @@ class App extends Component {
       }
     }
 
-    return [...merged.map(ev => ({ ...ev }))];
+    return merged;
   }
+
 
   slotPropGetter(time) {
     if (time < new Date()) {
@@ -78,9 +90,15 @@ class App extends Component {
     }
   }
 
+
+  eventsEqual(a, b) {
+    return ['end', 'start'].every(key => a[key].getTime() === b[key].getTime());
+  }
+
+
   render() {
-    if (first) {
-      first = false;
+    if (firstRender) {
+      firstRender = false;
 
       setTimeout(() => this.forceUpdate(), 100);
       
@@ -92,17 +110,17 @@ class App extends Component {
     return (
       <div className="App">
         <BigCalendar
-          defaultView={BigCalendar.Views.WORK_WEEK}
-          events={this.state.events}
-          localizer={localizer}
-          max={ new Date(1970, 1, 1, 20)}
-          min={ new Date(1970, 1, 1, 8)}
-          onSelectEvent={this.handleSelectEvent}
-          onSelectSlot={this.handleSelectSlot}
-          selectable={true}
-          slotPropGetter={this.slotPropGetter}
+          defaultView={ BigCalendar.Views.WORK_WEEK }
+          events={ this.state.events }
+          localizer={ localizer }
+          max={ new Date(1970, 1, 1, 21) }
+          min={ new Date(1970, 1, 1, 8) }
+          onSelectEvent={ this.handleSelectEvent }
+          onSelectSlot={ this.handleSelectSlot }
+          selectable
+          slotPropGetter={ this.slotPropGetter }
           style={{ margin: '50px 100px' }}
-          views={[BigCalendar.Views.WORK_WEEK]}
+          views={ [BigCalendar.Views.WORK_WEEK] }
         />
       </div>
     );
